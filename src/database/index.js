52672850905f060d293cf5b3e20cb99619736c62
Sérgio <71034184+Cangaceiro15@users.noreply.github.com>
2021-02-17 20:37:36 -0300

    New: Sequelize config Update: Folders structure

---
+++ src/database/index.js
@@ -0,0 +1,18 @@
+import sequelize from 'sequelize';
+import { Sequelize } from 'sequelize';
+
+import databaseConfig from './../config/database';
+
+const models = [];
+
+class Database {
+  constructor()
+  {
+    this.init();
+  }
+
+  init()
+  {
+    this.sequelize = new Sequelize(databaseConfig);
+  }
+}
