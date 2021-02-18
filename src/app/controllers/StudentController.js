bc231a286aa63c159660d1e63c98fba508d6b57f
Sérgio <71034184+Cangaceiro15@users.noreply.github.com>
2021-02-18 09:14:08 -0300

    Update: Cant create a student with an email of an existent student

--- src/app/controllers/StudentController.js
+++ src/app/controllers/StudentController.js
@@ -4,6 +4,10 @@ class StudentController {
   async store(req, res) {
     const { name, email, idade, peso, altura } = req.body;
 
+    if ((await Student.findOne({ where: { email } }))) {
+      return res.status(400).json({ error: 'Esse studante ja existe! Utilize outro endereco de email!' });
+    }
+
     const student = await Student.create({
       name, 
       email,
